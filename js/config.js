a3429dcf41c10f9217dbeb86aa8aca5447898be7
三三 <oott123@users.noreply.github.com>
2015-09-07 22:48:44 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,9 @@
 // array of Monitor-specific API keys or Main API key to list all monitors
 var __apiKeys = [
-	'm776858319-7c829d2c3e250f85776f588e', //BBS
-	'm776858325-1ddf7c7e98ce56609325db3b', //Doku
-	'm776858327-19b31e671f6b217b8ded96c6', //Me
-	'm776858407-a704cd548fb5c15db5b625f9', //Update
-	'm776858320-7b44f9ac136f69ac3e356e8e', //Auth A
-	'm776858321-486827042ed7a7f7b8952a05', //Auth B
-	'm776858322-0e2c3f842139c06f56f7f56a', //Auth C
-	'm776858323-0bdafb9e9a71e3305c27c1af', //Auth D
+	'm777069574-5d965cf7e35c8b6fac28e63e', // 主站
+	'm777069575-769dc9c507cb670a27c7479c', // 驿站
+	'm777069576-72f7b29e2479681590b82e0d', // 路由
+	'm777069579-bf5222c023af46681ecbc914', // 游戏
 ];
 
 // refresh interval (in seconds)
